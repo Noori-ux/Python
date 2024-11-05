c2cb62f6fb92311cebfb184d7c67475756835cf7
Noori-ux <gurnoorbajwa042007@gmail.com>
2024-11-04 21:49:44 -0500

    Create Calculating_averages.py
    First Python program

---
+++ Calculating_averages.py
@@ -0,0 +1,20 @@
+# This program calculates the sum of family member's age
+# Author: Noori
+
+
+
+
+ 
+myMotherAge = input("Mother's age: ")
+print(myMotherAge)
+
+myFatherAge = input("Father's age: ")
+print(myFatherAge)
+
+myDogAge = input("Dog's age: ")
+print(myDogAge)
+
+myAge = input("my own age: ")
+sum = int(myMotherAge) + int(myFatherAge) + int(myDogAge) + int(myAge)
+
+print("the total ages are: ", sum)

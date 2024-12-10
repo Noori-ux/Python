300a3070d5ad79adf6122e0507453ef0cde8e0a7
Noori-ux <gurnoorbajwa042007@gmail.com>
2024-12-09 20:46:52 -0500

    library demo
    A study about types, int, float, string (str), tuple, and set. Python memory scope.

---
+++ lib_demo1.py
@@ -0,0 +1,14 @@
+def fun():
+    print("I am inside fun")
+
+def fun1():
+    print("I am inside fun1")
+    
+a=1 #int
+b=2 #int
+name="Gurnoor" #string
+c="3" #string of one character
+d=1.5 #float
+l=[1,2,3] #list
+t=(1,2) #tuple
+di={name,c} #dictionary 
